626b85ff04641df80d05ff677f9e0827ade2fada
Evelina <evelinaavegas@gmail.com>
2025-10-21 13:37:44 +0300

    mobile v

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 import arrContent from "./arr.js"
 const list = document.querySelector('.list-wrapp')
-const contentBlock =document.querySelector('.content-wrapp')
+const contentBlock = document.querySelector('.content-wrapp')
 let listArr = ['verben', 'adjectives', 'nomen', 'ausgewählt']
 listArr.forEach(e => {
     let listBtn = document.createElement('div')
-    listBtn.classList.add( 'list-btn')
+    listBtn.classList.add('list-btn')
     listBtn.setAttribute('data-id', `${e}`)
     listBtn.textContent = `${e}`
     list.appendChild(listBtn)
@@ -16,24 +16,24 @@ function addEllement(htmlBlock, className) {
 }
 
 //  додавання блоків німецькою
-let listId =''
-let keyArr =[]
-let valueArr =[]
-list.addEventListener('click', e=>{
+let listId = ''
+let keyArr = []
+let valueArr = []
+list.addEventListener('click', e => {
     e.preventDefault()
     listId = e.target.dataset.id
     let selectArr = arrContent[listId]
     keyArr = Object.keys(selectArr)
     valueArr = Object.values(selectArr)
-    contentBlock.innerHTML=''
-    for(let i=0;i<keyArr.length;i++){
+    contentBlock.innerHTML = ''
+    for (let i = 0; i < keyArr.length; i++) {
         let arrElement = addEllement('div', 'content-el')
-        arrElement.textContent=`${keyArr[i]}` 
+        arrElement.textContent = `${keyArr[i]}`
         contentBlock.appendChild(arrElement)
     }
 })
 // додавання перекладу
-contentBlock.addEventListener('click', e=>{
+contentBlock.addEventListener('click', e => {
     e.preventDefault()
     let eTarget = e.target
     let targetContent = e.target.textContent
@@ -41,11 +41,10 @@ contentBlock.addEventListener('click', e=>{
     let indexKey = keyArr.indexOf(targetContent)
     let indexValue = valueArr.indexOf(targetContent)
 
-    if(targetContent== keyArr[indexKey]){
+    if (targetContent == keyArr[indexKey]) {
         arrContent.ausgewählt[targetContent] = valueArr[indexKey]
-        eTarget.textContent= `${valueArr[indexKey]}`
-    } else if(targetContent== valueArr[indexValue]){
-        eTarget.textContent= `${keyArr[indexValue]}`
+        eTarget.textContent = `${valueArr[indexKey]}`
+    } else if (targetContent == valueArr[indexValue]) {
+        eTarget.textContent = `${keyArr[indexValue]}`
     }
-    })
-    
+})
